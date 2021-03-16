d45d3c4be3f6567da7a2a951e4a162742d4c51e7
lmonsalve22 <lmonsalve22@gmail.com>
2021-03-16 12:57:30 -0300

    commit

--- src/Contenido/verificacion/src/dateFormat.test.ts
+++
@@ -1,5 +0,0 @@
-
-import CsvUtil from './util/CsvUtil';
-import TestSuite from './util/TestSuite';
-
-TestSuite.runForTag('ISO', file => CsvUtil.checkColumnDateFormat(file, 'yyyy-MM-dd'));
--- src/Contenido/verificacion/src/emptyLines.test.ts
+++
@@ -1,5 +0,0 @@
-
-import CsvUtil from './util/CsvUtil';
-import TestSuite from './util/TestSuite';
-
-TestSuite.runForAll(file => CsvUtil.checkNoEmptyLines(file));
